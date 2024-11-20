c0bef33e29cdbda9d267f210dcd6e06871523696
Krystian Sitarek <krystiansitarek@gmail.com>
2024-11-20 20:47:53 +0100

    MediatR

--- src/API/Heyer.Storage.API/Program.cs
+++ src/API/Heyer.Storage.API/Program.cs
@@ -1,10 +1,12 @@
+using System.Reflection;
 using Heyer.Storage.API.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddStorageStrategy(builder.Configuration.GetSection("StorageStrategy"));
+
+builder.Services.AddMediatR(c => c.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
