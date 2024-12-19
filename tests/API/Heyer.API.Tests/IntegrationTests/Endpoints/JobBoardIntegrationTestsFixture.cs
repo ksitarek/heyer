3d1e1e1e797b2f0ca2800bebe4f8b5344bf996a7
Krystian Sitarek <krystiansitarek@gmail.com>
2024-12-19 14:11:25 +0100

    Restored JobBoardIntegrationTestsFixture

--- tests/API/Heyer.API.Tests/IntegrationTests/Endpoints/JobBoardIntegrationTestsFixture.cs
+++ tests/API/Heyer.API.Tests/IntegrationTests/Endpoints/JobBoardIntegrationTestsFixture.cs
@@ -6,19 +6,19 @@ namespace Heyer.API.Tests.IntegrationTests.Endpoints;
 [SetUpFixture]
 public class JobBoardIntegrationTestsFixture
 {
-    // private MongoDbFixture _mongoDbFixture = new();
-    //
-    // [OneTimeSetUp]
-    // public async Task OneTimeSetUp()
-    // {
-    //     await _mongoDbFixture.InitializeAsync();
-    //
-    //     ApplicationFactoryConfiguration.InMemoryConfiguration[Config.MongoDb_ConnectionString] = _mongoDbFixture.ConnectionString;
-    // }
-    //
-    // [OneTimeTearDown]
-    // public async Task OneTimeTearDown()
-    // {
-    //     await _mongoDbFixture.DisposeAsync();
-    // }
+    private MongoDbFixture _mongoDbFixture = new();
+    
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        await _mongoDbFixture.InitializeAsync();
+    
+        ApplicationFactoryConfiguration.InMemoryConfiguration[Config.MongoDb_ConnectionString] = _mongoDbFixture.ConnectionString;
+    }
+    
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await _mongoDbFixture.DisposeAsync();
+    }
 }
