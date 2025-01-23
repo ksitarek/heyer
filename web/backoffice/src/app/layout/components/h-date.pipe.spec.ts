3eb984a756e6fdf0eec59505127df80e3a4052d8
Krystian Sitarek <krystiansitarek@gmail.com>
2025-01-23 16:47:29 +0100

    Husky

--- web/backoffice/src/app/layout/components/h-date.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HDatePipe } from './h-date.pipe';
-
-describe('HDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new HDatePipe();
-
-    expect(pipe).toBeTruthy();
-  });
-});
