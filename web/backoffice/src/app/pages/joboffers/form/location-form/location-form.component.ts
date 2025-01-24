0151df24aa50fb34e79674494b566ab40cc43656
Krystian Sitarek <krystiansitarek@gmail.com>
2025-01-24 20:26:09 +0100

    simplified subscription for saving location

--- web/backoffice/src/app/pages/joboffers/form/location-form/location-form.component.ts
+++ web/backoffice/src/app/pages/joboffers/form/location-form/location-form.component.ts
@@ -2,17 +2,7 @@ import { Component, input, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
-import {
-  combineLatest,
-  debounceTime,
-  distinct,
-  filter,
-  map,
-  Observable,
-  startWith,
-  Subscription,
-  switchMap,
-} from 'rxjs';
+import { debounceTime, distinct, filter, Observable, Subscription, switchMap, tap } from 'rxjs';
 import { JobOfferLocationService } from './joboffer-location.service';
 
 @Component({
@@ -24,39 +14,34 @@ import { JobOfferLocationService } from './joboffer-location.service';
 export class LocationFormComponent implements OnInit, OnDestroy {
   readonly form = input.required<FormGroup>();
 
-  private locationChanged$!: Subscription;
+  private locationChangedSubscription!: Subscription;
 
   constructor(private JobOfferLocationService: JobOfferLocationService) {}
 
   public ngOnInit(): void {
     const idControl = this.form().get('id');
-    const cityControl = this.form().get('location.city');
-    const countryControl = this.form().get('location.country');
+    const location = this.form().get('location');
 
-    const cityControl$ = cityControl?.valueChanges as Observable<string>;
-    const countryControl$ = countryControl?.valueChanges as Observable<string>;
+    const location$ = location?.valueChanges as Observable<{ city: string; country: string }>;
 
-    this.locationChanged$ = combineLatest([
-      cityControl$.pipe(startWith(cityControl?.value as string)),
-      countryControl$.pipe(startWith(countryControl?.value as string)),
-    ])
+    this.locationChangedSubscription = location$
       .pipe(
         debounceTime(500),
         distinct(),
 
-        map(([city, country]) => ({ city, country })),
-
-        filter(() => (cityControl?.dirty ?? false) || (countryControl?.dirty ?? false)),
+        filter(() => location?.dirty ?? false),
         filter(({ city, country }) => city.length > 0 && country.length > 0),
 
         switchMap(({ city, country }) =>
           this.JobOfferLocationService.setOfficeLocation(idControl?.value as string, city, country),
         ),
+
+        tap(() => location?.markAsPristine()),
       )
       .subscribe();
   }
 
   public ngOnDestroy(): void {
-    this.locationChanged$.unsubscribe();
+    this.locationChangedSubscription.unsubscribe();
   }
 }
