0bb9e3941d4b4454c894be0e03bef7ce5e252a39
Krystian Sitarek <krystiansitarek@gmail.com>
2025-02-04 18:14:52 +0100

    Modules installation & use redone (#4)

--- src/BuildingBlocks/Heyer.BuildingBlocks.Application/HttpLanguage/FilteredListRequest.cs
+++ src/BuildingBlocks/Heyer.BuildingBlocks.Application/HttpLanguage/FilteredListRequest.cs
@@ -4,21 +4,3 @@ public record FilteredListRequest(int Page, int PageSize, SortRequest? Sort)
 {
     public int PageIx => Page - 1;
 }
-
-public record SortRequest(string By, SortOrder Order)
-{
-    public static SortRequest From(string by, string order) =>
-        new(by,
-            order.ToLowerInvariant() switch
-            {
-                "asc" => SortOrder.Ascending,
-                "desc" => SortOrder.Descending,
-                _ => throw new ArgumentException("Invalid sort order")
-            });
-}
-
-public enum SortOrder
-{
-    Ascending = 0,
-    Descending = 1
-}
--- src/Modules/Hiring/Heyer.Modules.Hiring.Infrastructure/HiringModule.cs
+++ src/Modules/Hiring/Heyer.Modules.Hiring.Infrastructure/HiringModule.cs
@@ -1,78 +1,12 @@
-using FluentValidation;
-using Hangfire;
-using Heyer.BuildingBlocks.Application.Authorization;
-using Heyer.BuildingBlocks.Infrastructure;
-using Heyer.BuildingBlocks.Infrastructure.Integration;
-using Heyer.BuildingBlocks.Infrastructure.Mediator;
-using Heyer.BuildingBlocks.Infrastructure.Mediator.Middleware;
-using Heyer.BuildingBlocks.Infrastructure.Messaging;
 using Heyer.BuildingBlocks.Infrastructure.Modules;
 using Heyer.Modules.Hiring.Application;
-using Heyer.Modules.Hiring.Infrastructure.Configuration;
-using Heyer.Modules.Hiring.Infrastructure.Integration;
-using Heyer.Storage.API.Client;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Heyer.Modules.Hiring.Infrastructure;
 
-public class HiringModule : ModuleRunner, IHiringModule
+internal class HiringModule : ModuleRunner, IHiringModule
 {
-    private readonly IEventBus _eventBus;
-
-    public HiringModule(IConfiguration configuration, IEventBus eventBus)
+    public HiringModule(Func<IServiceScope> scopeProvider) : base(scopeProvider)
     {
-        _eventBus = eventBus;
-
-        var services = new ServiceCollection();
-
-        ConfigureServices(configuration, services);
-
-        HiringModuleCompositionRoot.SetServiceProvider(services.BuildServiceProvider());
-    }
-
-    public override Func<IServiceScope> ScopeProvider => HiringModuleCompositionRoot.CreateScope;
-
-    public void ConfigureModule(WebApplication app)
-    {
-        HiringEndpointsConfiguration.MapEndpoints(app);
-
-        ConfigureScheduler(app);
-    }
-
-    private static void ConfigureScheduler(WebApplication app)
-    {
-        var recurringJobManager = app.Services.GetRequiredService<IRecurringJobManager>();
-        recurringJobManager.AddOrUpdate<HiringInboxProcessingJob>(
-            $"{nameof(HiringModule)}_InboxProcessing",
-            job => job.Handle(),
-            "* * * * * *");
-        recurringJobManager.AddOrUpdate<HiringOutboxProcessingJob>(
-            $"{nameof(HiringModule)}_OutboxProcessing",
-            job => job.Handle(),
-            "* * * * * *");
-    }
-
-    private void ConfigureServices(IConfiguration configuration, ServiceCollection services)
-    {
-        services.AddSingleton(_eventBus);
-
-        var assembly = typeof(HiringEndpointsConfiguration).Assembly;
-
-        services
-            .AddSingleton<IDateTimeProvider, SystemDateTime>()
-            .AddMediator(assembly,
-                         typeof(LoggingMiddleware<,>),
-                         typeof(ValidationMiddleware<,>),
-                         typeof(UnitOfWorkMiddleware<,>))
-            .AddStorageApiClient(configuration["StorageApi:Url"])
-            .AddDomainEventDispatcher(assembly)
-            .AddUserDataProvider();
-
-        services
-            .AddValidatorsFromAssembly(assembly)
-            .AddHiringDbContext(configuration.GetSection("Companies"))
-            .AddPersistence();
     }
 }
