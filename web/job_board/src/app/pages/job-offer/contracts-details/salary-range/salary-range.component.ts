452107eb3f1125b0a5dee14a8b5ae668b91f9646
Krystian Sitarek <krystiansitarek@gmail.com>
2025-02-07 10:53:27 +0100

    redesign

--- web/job_board/src/app/pages/job-offer/contracts-details/salary-range/salary-range.component.ts
+++ web/job_board/src/app/pages/job-offer/contracts-details/salary-range/salary-range.component.ts
@@ -1,10 +1,9 @@
 import { Component, input } from '@angular/core';
-import { HlmH2Directive } from '@spartan-ng/ui-typography-helm';
 import { SalaryRange } from '../../../../models/salary-range.model';
 
 @Component({
   selector: 'h-salary-range',
-  imports: [HlmH2Directive],
+  imports: [],
   templateUrl: './salary-range.component.html',
   styleUrl: './salary-range.component.scss',
 })
